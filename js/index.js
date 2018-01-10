aed2b1e2cdefc7d1180e3e568fe54ed326488a9f
Julia John <jjuli90@gmail.com>
2018-01-10 16:06:30 +0100

    küzdelem a vue-bootstrap-megjelenítés szentháromsággal

--- js/index.js
+++ js/index.js
@@ -1,80 +1,80 @@
 Vue.use(Vuetable);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   components: {
-   'vuetable-pagination': Vuetable.VuetablePagination
+    "vuetable-pagination": Vuetable.VuetablePagination
   },
   data: {
     fields: [
       {
-        name: 'name',
-        title: '<span class="glyphicon glyphicon-sort-by-alphabet"></span> Barlang neve',
-        sortField: 'name'
+        name: "name",
+        title:
+          '<span class="glyphicon glyphicon-sort-by-alphabet"></span> Barlang neve',
+        sortField: "name"
       },
       {
-        name: 'email',
-        title: 'Email',
-        sortField: 'email'
+        name: "email",
+        title: "Email",
+        sortField: "email"
       },
-      'birthdate','nickname',
+      "birthdate",
+      "nickname",
       {
-        name: 'gender',
-        title: 'Gender',
-        sortField: 'gender'
+        name: "gender",
+        title: "Gender",
+        sortField: "gender"
       },
-      '__slot:actions'
+      "__slot:actions"
     ],
-    sortOrder: [
-      { field: 'name', direction: 'asc' }
-    ],
+    sortOrder: [{ field: "name", direction: "asc" }],
     css: {
       table: {
-        tableClass: 'table table-striped table-bordered table-hovered',
-        loadingClass: 'loading',
-        ascendingIcon: 'glyphicon glyphicon-chevron-up',
-        descendingIcon: 'glyphicon glyphicon-chevron-down',
-        handleIcon: 'glyphicon glyphicon-menu-hamburger'
+        tableClass: "table table-striped table-bordered table-hovered",
+        loadingClass: "loading",
+        ascendingIcon: "glyphicon glyphicon-chevron-up",
+        descendingIcon: "glyphicon glyphicon-chevron-down",
+        handleIcon: "glyphicon glyphicon-menu-hamburger"
       },
       pagination: {
-        infoClass: 'pull-left',
-        wrapperClass: 'vuetable-pagination pull-right',
-        activeClass: 'btn-primary',
-        disabledClass: 'disabled',
-        pageClass: 'btn btn-border',
-        linkClass: 'btn btn-border',
+        infoClass: "pull-left",
+        wrapperClass: "vuetable-pagination pull-right",
+        activeClass: "btn-primary",
+        disabledClass: "disabled",
+        pageClass: "btn btn-border",
+        linkClass: "btn btn-border",
         icons: {
-          first: '',
-          prev: '',
-          next: '',
-          last: ''
-        },
+          first: "",
+          prev: "",
+          next: "",
+          last: ""
+        }
       }
     }
   },
-  computed:{
-  /*httpOptions(){
+  computed: {
+    /*httpOptions(){
     return {headers: {'Authorization': "my-token"}} //table props -> :http-options="httpOptions"
   },*/
- },
- methods: {
-    onPaginationData (paginationData) {
-      this.$refs.pagination.setPaginationData(paginationData)
+  },
+  methods: {
+    onPaginationData(paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData);
     },
-    onChangePage (page) {
-      this.$refs.vuetable.changePage(page)
+    onChangePage(page) {
+      this.$refs.vuetable.changePage(page);
     },
-    editRow(rowData){
-      alert("You clicked edit on"+ JSON.stringify(rowData))
+    editRow(rowData) {
+      alert("You clicked edit on" + JSON.stringify(rowData));
     },
-    deleteRow(rowData){
-      alert("You clicked delete on"+ JSON.stringify(rowData))
+    deleteRow(rowData) {
+      alert("You clicked delete on" + JSON.stringify(rowData));
     },
     onLoading() {
-      console.log('loading... show your spinner here')
+      console.log("loading... show your spinner here");
     },
     onLoaded() {
-      console.log('loaded! .. hide your spinner here')
+      console.log("loaded! .. hide your spinner here");
     }
   }
-})
+});
